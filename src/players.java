acc23244ab550b99148e2f874db84f2f44c807c8
Khaloody12 <khalid0552009@hotmail.com>
2021-07-13 22:51:56 +0300

    Initial commit

---
+++ src/players.java
@@ -0,0 +1,28 @@
+
+public class players {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+	
+	public void player() {
+		
+	}
+	
+
+}
+
+
+
+class player1 extends players{
+	
+	
+	
+	
+}
+
+
+class player2 extends players{
+	
+	
+	
+}
